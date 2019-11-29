ec1685b3f44c75ebd02e8e517ad0cf36c5b0932a
nick_richardson <nick.richardson@rezonence.com>
2019-11-29 14:44:31 +0000

    create animation

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -13,7 +13,32 @@ export class LayoutComponent implements OnInit {
 
   changeOrder() {
     const indices = [1, 2].map(() => this.getRandomIndex(0, (this.letters.length - 1)));
-    [this.letters[indices[0]], this.letters[indices[1]]] = [this.letters[indices[1]], this.letters[indices[0]]];
+    this.animateChange(indices);
+    // [this.letters[indices[0]], this.letters[indices[1]]] = [this.letters[indices[1]], this.letters[indices[0]]];
+  }
+
+  animateChange(indices: number[]) {
+    const tiles = indices.map(i => document.querySelector(`#letter-${this.letters[i]}`));
+    tiles.forEach((tile, index) => {
+      const startPosition = tile.getBoundingClientRect();
+      const endPosition = tiles[index === 0 ? 1 : 0].getBoundingClientRect();
+      const deltaX = startPosition.left - endPosition.left;
+      const deltaY = startPosition.top - endPosition.top;
+      tile.animate([
+        {
+          transformOrigin: 'top left',
+          transform: `translate(${deltaX}px, ${deltaY}px)`
+        },
+        {
+          transformOrigin: 'top left',
+          transform: 'none'
+        }
+      ], {
+        duration: 300,
+        easing: 'ease-in-out',
+        fill: 'both'
+      });
+    });
   }
 
   getRandomIndex(min: number, max: number): number {
